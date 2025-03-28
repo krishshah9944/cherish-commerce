e9549a94582cb44a28a4a678392015a77d06b28d
krishshah9944 <153007529+krishshah9944@users.noreply.github.com>
2025-03-28 12:55:51 +0530

    Delete

--- src/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vjdfsueamhqmjiqzrxgu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqZGZzdWVhbWhxbWppcXpyeGd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MTEyOTcsImV4cCI6MjA1Njk4NzI5N30.rfTJLxwkJ7TrzPmzBD89Nhv0TSSkV5ObBvb19eq_kPQ';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
